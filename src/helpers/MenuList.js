62c06c83ac0af52f131b14774eab36eac3b2c29f
one <babaalimohamed44@gmail.com>
2025-03-21 16:55:47 +0000

    first commit

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Chole from "../assets/couscous.jpeg";
+import Dosa from "../assets/tangya.jpeg";
+import Paneer from "../assets/tacous.jpeg";
+import Idli from "../assets/rfissat.jpeg";
+import Gujrati from "../assets/kabab.jpeg";
+import Rajasthani from "../assets/chicen.jpeg";
+
+export const MenuList = [
+  {
+    name: "Couscous",
+    image: Chole,
+    price: 250,
+  },
+  {
+    name: "Tangya",
+    image: Dosa,
+    price: 280,
+  },
+  {
+    name: "tacous",
+    image: Paneer,
+    price: 350,
+  },
+  {
+    name: "Rfissa",
+    image: Idli,
+    price: 230,
+  },
+  {
+    name: "Kabab",
+    image: Gujrati,
+    price: 450,
+  },
+  {
+    name: "Chicken",
+    image: Rajasthani,
+    price: 499,
+  },
+];
